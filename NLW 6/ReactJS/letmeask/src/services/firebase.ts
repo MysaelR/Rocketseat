4614a2aa4814bc3e5154f5368a396177890fdbd0
MysaelR <90765797+MysaelR@users.noreply.github.com>
2022-04-22 18:51:21 -0300

    Concluindo config do firebase, inicializando home da página

--- NLW 6/ReactJS/letmeask/src/services/firebase.ts
+++ NLW 6/ReactJS/letmeask/src/services/firebase.ts
@@ -2,16 +2,14 @@ import firebase, { initializeApp } from 'firebase/app';
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database"
 
-import 'firebase/database'
-
 const firebaseConfig = {
-    apiKey: "AIzaSyCEpH4iKGmZkB2vPc_ArydiyIwg3DdGFGQ",
-    authDomain: "letmeask-4e187.firebaseapp.com",
-    databaseURL: "https://letmeask-4e187-default-rtdb.firebaseio.com",
-    projectId: "letmeask-4e187",
-    storageBucket: "letmeask-4e187.appspot.com",
-    messagingSenderId: "650363454020",
-    appId: "1:650363454020:web:a5000a979726e89bbc3211"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
   
   // Initialize Firebase
