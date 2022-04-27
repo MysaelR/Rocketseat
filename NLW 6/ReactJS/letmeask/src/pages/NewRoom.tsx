5089c14d5c5a64eb43a9d5eb1a5bcaf2fb15667e
MysaelR <90765797+MysaelR@users.noreply.github.com>
2022-04-27 18:07:06 -0300

    Adicionando criação de nova sala

--- NLW 6/ReactJS/letmeask/src/pages/NewRoom.tsx
+++ NLW 6/ReactJS/letmeask/src/pages/NewRoom.tsx
@@ -1,11 +1,11 @@
-import googleIconImg from '../assets/google-icon.svg';
 import IlustrationImg from '../assets/illustration.svg';
 import logoImg from '../assets/logo.svg';
+import googleIconImg from '../assets/google-icon.svg';
+import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
-import '../styles/auth.scss';
 
-export function Home() {
+export function NewRoom() {
     return (
         <div id='page-auth'>
             <aside>
@@ -16,18 +16,17 @@ export function Home() {
             <main>
                 <div className='main-content'>
                     <img src={logoImg} alt="logo Letmeask" />
-                    <button className='create-room'>
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className='separator'>Ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>
                     <form>
                         <input
                             type="text"
-                            placeholder='Digite o código da sala'
+                            placeholder='Nome da sala'
                         />
-                        <Button type='submit'>Entrar na sala</Button>
+                        <Button type='submit'>Criar sala</Button>
                     </form>
+                    <p>
+                        Quer entrar em uma sala existente? <a href='#'>clique aqui</a>
+                    </p>
                 </div>
 
             </main>
